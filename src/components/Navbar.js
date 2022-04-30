6b99d9b58d4decdca0a77609d6000cec5b2314a6
Raymond Truong <rtru0006@student.monash.edu>
2022-04-30 13:44:04 +1000

    responsive nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,35 @@
-import React from "react";
+import React, { useRef } from "react";
 import "../styles/Navbar.css";
+import { FaBars, FaTimes } from "react-icons/fa";
+
 
 export default function Navbar() {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  }
+
   return (
-    <nav>
-      <div className="logo">
-        <h4>Raymond Truong</h4>
-      </div>
-      <ul className="navlinks">
-        <li className="link">
-          <a href="#">home</a>
-        </li>
-        <li className="link">
-          <a href="#">about</a>
-        </li>
-        <li className="link">
-          <a href="#">resume</a>
-        </li>
-        <div className="menu-toggle">
-          <i className="fas fa-bars fa-lg"></i>
+      <nav>
+        <div className="logo">
+          <h4>Raymond Truong</h4>
         </div>
-      </ul>
-    </nav>
+
+        <ul className="navlinks" ref={navRef}>
+          <li className="link">
+            <a href="#">home</a>
+          </li>
+          <li className="link">
+            <a href="#">about</a>
+          </li>
+          <li className="link">
+            <a href="#">resume</a>
+          </li>
+        </ul>
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <FaBars />
+        </button>
+      </nav>
   );
 }
